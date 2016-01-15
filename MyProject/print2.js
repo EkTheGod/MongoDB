5b99dcfcde35687df00c9099d19e4cbdbe38423d
EkTheGod <ekachart_ek@hotmail.com>
2016-01-15 18:32:26 +0700

    paging send cut data

--- MyProject/print2.js
+++ MyProject/print2.js
@@ -4,11 +4,8 @@ var url = 'mongodb://localhost:27017/book';
 
 var express = require('express');
 var path = require('path');
-var app = express();
+var app = express();  
 
-
-var fs = require('fs');
-var outputFilename = '/Dev/MyProject/my.js';
 var collection;
 var mydata;
 
@@ -25,13 +22,46 @@ MongoClient.connect(url, function (err, db, req, res) {
 });//end connect
 
 app.get('/data', function ( req, res) {
-    collection.find().toArray(function(err, articles) {
-      return res.send(articles);
+    collection.find().sort( { Book_ID: 1 } ).toArray(function(err, articles) {
+      res.send(articles);
+      //console.log(articles.length);
     });
   });
 
+app.post('/senddata', function ( req, res) {
+
+  
+
+  /*
+    var start = 0;
+    var dataperpage = 5;
+    var i=1;
+    var len;
+    collection.find().toArray(function(err, articles) {
+        len = articles.length;
+    });
+
+    while( (i*dataperpage) <=len){
+        collection.find().sort( { Book_ID: 1 } ).skip(start).limit(dataperpage).toArray(function(err, articles){
+            res.send(articles);
+        });
+        start = dataperpage*i - 1;
+
+    }
+    */
+});
+
+/*
+app.get('/datacount', function ( req, res) {
+    collection.find().toArray(function(err, articles) {
+      var l = articles.length;
+      res.json(l);
+    });
+});
+*/
+
 app.get('/', function ( req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'paging.html'));
   });
  
 var server = app.listen(5555, function() {
--- MyProject/print3.js
+++ MyProject/print3.js
@@ -12,7 +12,7 @@ var assert = require('assert');
 var fs = require('fs');
 
 var collection;
-var mydata;
+var mydata,status,i;
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -34,6 +34,12 @@ app.get('/data', function ( req, res) {
     });
 });
 
+app.post('/data', function ( req, res) {
+    collection.find().sort( { Book_ID: 1 } ).toArray(function(err, articles) {
+      return res.send(articles);
+    });
+});
+
 app.get('/', function ( req, res) {
     res.sendFile(path.join(__dirname, 'data management.html'));
   });
@@ -41,6 +47,7 @@ app.get('/', function ( req, res) {
 app.get('/show', function ( req, res) {
     res.sendFile(path.join(__dirname, 'paging.html'));
   });
+
 
 
 app.post('/insert', function (req, res) {
@@ -57,59 +64,47 @@ app.post('/insert', function (req, res) {
             collection.findOne( { Book_Name : name } , function(err, articles2) {
                 findName = articles2;
         
-        
             if( findId == null && findName == null)
             {   collection.insert({ Book_ID:req.body.Book_ID , Book_Name:req.body.Book_Name, Date:req.body.Date });
                 res.sendFile(path.join(__dirname, 'data management.html'));
-                return 'Insert : Success';
-            }
+            } 
             else
-            {   console.log('Allready Have It');
                 res.sendFile(path.join(__dirname, 'data management.html'));
-            }
-        
-            });
-        });
-
+            });//articles2
+        });//articles
   }
-  else if(req.body.Book_ID == "" || req.body.Book_Name == "" || req.body.Date == "")
-        {   
-            res.sendFile(path.join(__dirname, 'data management.html')); 
-            return 'You dont enter all';
-        }
-
+  else 
+        res.sendFile(path.join(__dirname, 'data management.html')); 
 });
+
 
 app.post('/remove', function (req, res) {
 
   var id = req.body.Book_ID_r;
   var name = req.body.Book_Name_r;
-  var findId,findName;
+  var findId;
 
   if( req.body.Book_ID_r != "" && req.body.Book_Name_r != "" && req.body.Date_r != "" )
   {   
       collection.findOne( { Book_ID : id , Book_Name : name } , function(err, articles) {
             findId = articles;
         
-            if( findId != null )
-            {   collection.remove({ Book_ID:req.body.Book_ID_r , Book_Name:req.body.Book_Name_r });
-                console.log('Success')
-                res.sendFile(path.join(__dirname, 'data management.html'));
-            }
-            else
-            {   console.log('Not found in Database');
-                res.sendFile(path.join(__dirname, 'data management.html'));
-            }
+                if( findId != null )
+                {   collection.remove({ Book_ID:req.body.Book_ID_r , Book_Name:req.body.Book_Name_r });
+                    //console.log('Success')
+                    res.sendFile(path.join(__dirname, 'data management.html'));
+                }
+                else
+                {   //console.log('Not found in Database');
+                    res.sendFile(path.join(__dirname, 'data management.html'));
+                }
       });
   }
   else 
-        {   console.log('You dont enter all');
+        {   //console.log('You dont enter all');
             res.sendFile(path.join(__dirname, 'data management.html')); 
         }
-
 });
-
-/*
 
 app.post('/update', function (req, res) {
 
@@ -117,6 +112,9 @@ app.post('/update', function (req, res) {
   var name = req.body.Book_Name_u;
   console.log( id+' , '+name );
   console.log( req.body.New_book_ID+' , '+req.body.New_book_Name+' , '+req.body.New_Date );
+  res.sendFile(path.join(__dirname, 'data management.html')); 
+
+  /*
 
   if( req.body.Book_ID_u != "" && req.body.Book_Name_u != "" && req.body.Date_u != "" )
   {   
@@ -142,11 +140,9 @@ app.post('/update', function (req, res) {
         {   console.log('You dont enter all');
             res.sendFile(path.join(__dirname, 'data management.html')); 
         }
-
+  */
 });
-
-*/
  
-var server = app.listen(5555, function() {
+var server = app.listen(5556, function() {
     console.log('Web is running...');
 });
